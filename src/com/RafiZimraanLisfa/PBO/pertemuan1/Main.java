e68f0dc55eec46e60280bf30e217c7dc5da00ebf
rafizimraan <21102039@ittelkom-pwt.ac.id>
2023-05-02 14:51:35 +0700

    Initial commit

---
+++ src/com/RafiZimraanLisfa/PBO/pertemuan1/Main.java
@@ -0,0 +1,10 @@
+package com.RafiZimraanLisfa.PBO.pertemuan1;
+
+public class Main {
+    // psvm
+    public static void main(String[] args) {
+        // sout
+        System.out.println("Hello Word");
+        System.out.println("Rafi Zimraan");
+    }
+}
---
+++ src/com/RafiZimraanLisfa/PBO/pertemuan2/Variables.java
@@ -0,0 +1,11 @@
+package com.RafiZimraanLisfa.PBO.pertemuan2;
+
+public class Variables {
+    public static void main(String[] args) {
+        String nama = "Rafi Zimraan Lisfa";
+        int umur = 19;
+
+        System.out.println("Nama Saya : " + nama);
+        System.out.println("Umur Saya : " + umur);
+    }
+}
